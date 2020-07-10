6845ec0ef18c8bd6bbb2f8f76b387acd9511699e
xiechangwang <1113868253@qq.com>
2020-07-10 17:08:46 +0800

    day-01/dome-01.py

---
+++ day-01/dome-01.py
@@ -0,0 +1,19 @@
+print('hello word');
+print('python 的第一天吧'); #python必须换行
+
+#变量
+name="张三"
+age=12
+floatNum=3.26
+print(name)
+print(age)
+print(floatNum)
+
+
+#变量-命名规则
+#命名规则-见名知意
+
+import keyword
+print(keyword.kwlist)
+#['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+#以上是python关键字
